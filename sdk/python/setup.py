9bbcf269d4ca6f18e78fa8965b46f882a2c0bbe6
root <root@DESKTOP-35L2V1A.localdomain>
2022-06-07 10:25:46 -0700

    fixed malformed type tokens

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -8,8 +8,8 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "0.0.0"
-PLUGIN_VERSION = "0.0.0"
+VERSION = "0.1.4"
+PLUGIN_VERSION = "0.1.4"
 
 class InstallPluginCommand(install):
     def run(self):
