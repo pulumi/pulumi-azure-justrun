77bb04109c4138cd25bcd0b89db290f828abf4b7
root <root@DESKTOP-35L2V1A.localdomain>
2022-06-07 14:20:39 -0700

    reverted nodejs changes

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -8,8 +8,8 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "0.1.8"
-PLUGIN_VERSION = "0.1.8"
+VERSION = "0.1.9"
+PLUGIN_VERSION = "0.1.9"
 
 class InstallPluginCommand(install):
     def run(self):
