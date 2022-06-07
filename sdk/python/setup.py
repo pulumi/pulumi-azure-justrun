222d489a6d3c4bf127f48aecffb91d175370bbac
root <root@DESKTOP-35L2V1A.localdomain>
2022-06-07 13:57:43 -0700

    bumped version

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -8,8 +8,8 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = "0.1.7"
-PLUGIN_VERSION = "0.1.7"
+VERSION = "0.1.8"
+PLUGIN_VERSION = "0.1.8"
 
 class InstallPluginCommand(install):
     def run(self):
