d2424f1fc412e70c200c2e31477aebf8d68b0d4e
Harry Liuson <hliuson@Hotmail.com>
2022-06-10 10:43:14 -0700

    new example

---
+++ examples/cswebapp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Pulumi;
+using Pulumi.AzureJustrun;
+using System.Collections.Generic;
+
+await Pulumi.Deployment.RunAsync(() =>
+{
+    // Create an Azure Resource Group
+    var resourceGroup = new WebApp("myapp", new WebAppArgs{
+        FilePath = "./www"
+    });
+
+    // Export the primary key of the Storage Account
+    return new Dictionary<string, object?>
+    {
+        ["url"] = resourceGroup.url
+    };
+});
