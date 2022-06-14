9f379e69b330c479480adaf1c2cc864c58482f07
Harry Liuson <hliuson@Hotmail.com>
2022-06-14 11:58:26 -0700

    removed python ex

--- examples/python-ex/__main__.py
+++
@@ -1,9 +0,0 @@
-"""An Azure RM Python Pulumi program"""
-
-import pulumi
-import pulumi_azure_justrun 
-
-
-webapp = pulumi_azure_justrun.Webapp("mywebapp", file_path="./www")
-
-pulumi.export("url",webapp.url)
