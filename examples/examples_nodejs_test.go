44850325212cb98dd0a420e1df87149e672f4196
Harry Liuson <hliuson@Hotmail.com>
2022-06-15 09:15:34 -0700

    add containerapp test

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -16,11 +16,17 @@ func TestWebAppNodeJS(t *testing.T) {
 	integration.ProgramTest(t, &test)
 }
 
+func TestContainerAppNodeJS(t *testing.T) {
+	test := getJSBaseOptions(t).With(integration.ProgramTestOptions{
+		Dir: filepath.Join(getCwd(t), "nodejscontainerapp"),
+	})
+	integration.ProgramTest(t, &test)
+}
+
 func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	base := getBaseOptions()
 	baseJS := base.With(integration.ProgramTestOptions{
-		Dependencies: []string{
-		},
+		Dependencies: []string{},
 	})
 
 	return baseJS
